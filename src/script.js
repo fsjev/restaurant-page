90a4f75e8c799db80b803e97be21a04ed8060a73
Jevaughn Vickers <fullstackjev@gmail.com>
2023-08-26 12:05:37 -0400

    write function for active tab indication

--- src/script.js
+++ src/script.js
@@ -1,4 +1,29 @@
-import renderElement from "./page-load";
-import homeModule from "./home-tab";
+// import renderElement from "./page-load";
+// import homeModule from "./home-tab";
+// import menuModule from "./menu-tab";
+// import contactModule from "./contact-tab";
 
-renderElement(homeModule());
+// renderElement(menuModule());
+
+const tabButtons = {
+    home: document.getElementById("home-tab"),
+    menu: document.getElementById("menu-tab"),
+    contact: document.getElementById("contact-tab")
+};
+
+tabButtons.home.addEventListener("click", highlightTab);
+tabButtons.menu.addEventListener("click", highlightTab);
+tabButtons.contact.addEventListener("click", highlightTab);
+
+
+function highlightTab(e){
+    e.target.setAttribute("class", "active");
+    let otherBtns = [];
+    Array.from(tabButtons.home.parentNode.parentNode.children).forEach(elem => {
+        if(elem.childNodes[0] !== e.target){
+            otherBtns.push(elem.childNodes[0]);
+        };
+    });
+    otherBtns.forEach(btn => btn.removeAttribute("class", "active"))
+};
+
